7b9eee7db6270a065fdaa3f71db69d0405597128
sonaliii <sona8242@gmail.com>
2020-07-05 11:19:55 -0700

    added docstring

--- src/data_loading.py
+++ src/data_loading.py
@@ -2,5 +2,10 @@ import pandas as pd
 
 
 def load_data(csv_path):
+    """
+    Load input CSV into a pandas DataFrame
+    :param csv_path: str absolute path to csv file
+    :return: pandas DataFrame with input data
+    """
     df = pd.read_csv(csv_path)
     return df
